1e4b9038b1cb21c33c2e12de84bf86d93f646a0b
mahedi522 <mahediraj91@gmail.com>
2023-07-26 10:54:57 +0600

    Front End Done

--- AngularAuthUI/src/app/components/signup/signup.component.ts
+++ AngularAuthUI/src/app/components/signup/signup.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
 
   type: string = "password"
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash";
+  constructor(){}
+   ngOnInit(): void{
 
-  ngOnInit(){}
+   }
 
   hideShowPass(){
     this.isText = !this.isText;
